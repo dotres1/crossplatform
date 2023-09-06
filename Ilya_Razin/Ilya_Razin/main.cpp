8704baec56cf1bd27cf9fb7bc942187bb116bba3
dotres1 <140379286+dotres1@users.noreply.github.com>
2023-09-06 23:30:35 +0300

    project template

---
+++ Ilya_Razin/Ilya_Razin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ilya_Razin
+//
+//  Created by Илья on 06.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
